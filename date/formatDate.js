becc0e5e3cd25142b45912883c2665fe3a105389
biop72231 <35953235+biop72231@users.noreply.github.com>
2018-12-13 15:15:55 +0800

    Update formatDate.js

--- date/formatDate.js
+++ date/formatDate.js
@@ -6,6 +6,6 @@ function addZero(i) {
 }
 function formatDate(d,t){
   let d = new Date();
-  let time = d.getFullYear() + d + addZero(d.getMonth()) + 1 + d + addZero(d.getDate()) + ' ' + addZero(d.getHours()) + t + addZero(d.getMinutes()) + t + addZero(d.getSeconds());
+  let time = d.getFullYear() + d + addZero(d.getMonth() + 1 ) + d + addZero(d.getDate()) + ' ' + addZero(d.getHours()) + t + addZero(d.getMinutes()) + t + addZero(d.getSeconds());
   return time;
 }
