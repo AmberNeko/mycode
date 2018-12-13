157c0f7cd49c7f76669a9e6fd918e5637aa0f427
biop72231 <35953235+biop72231@users.noreply.github.com>
2018-12-13 15:04:10 +0800

    更改變數

--- date/formatDate.js
+++ date/formatDate.js
@@ -1,5 +1,5 @@
 function formatDate(d,t){
   let d = new Date();
-  let t = d.getFullYear() + d + d.getMonth() + 1 + d + d.getDate() + ' ' + d.getHours() + t + d.getMinutes() + t + d.getSeconds();
-  return t;
+  let time = d.getFullYear() + d + d.getMonth() + 1 + d + d.getDate() + ' ' + d.getHours() + t + d.getMinutes() + t + d.getSeconds();
+  return time;
 }
