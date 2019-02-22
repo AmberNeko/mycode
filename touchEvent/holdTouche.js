0ec6c324b06ec2a96bc84436ab073fc456951995
biop72231 <35953235+biop72231@users.noreply.github.com>
2019-02-22 14:20:07 +0800

    holdTouch decide
    按鈕長按判斷

---
+++ touchEvent/holdTouche.js
@@ -0,0 +1,14 @@
+var timer
+$(target).on({
+  touchstart: function(){
+    timer = setTimeout(function(){
+      //do what?
+    },1000)
+  },
+  touchend: function(){
+    clearTimeout(timer);
+  },
+  touchmove: function(){
+    clearTimeout(timer);
+  }
+});
