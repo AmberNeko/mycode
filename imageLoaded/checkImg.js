ff3120b7c7ff4501ec51b44e6b81389e3bf3a7f5
biop72231 <35953235+biop72231@users.noreply.github.com>
2019-02-22 14:50:25 +0800

    decide image is loaded
    判斷圖片讀取完畢

---
+++ imageLoaded/checkImg.js
@@ -0,0 +1,6 @@
+function checkImg() {
+        if (document.querySelectorAll(target)[0].complete) {
+            bodyHeight = $('body').height();
+            bodyWidth = $('body').width();
+        }
+    }
